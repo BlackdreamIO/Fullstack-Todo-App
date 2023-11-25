ed359a3abf5f6d4eedc47288f631091f744f38b8
MohammedHamim <99599371+BlackdreamIO@users.noreply.github.com>
2023-11-26 00:13:40 +0600

    MUI COMMIT

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Button = ({text, useTransition=true, ...props}) => {
     return (
         <button 
-            className={`dark:bg-black dark:hover:bg-white m-1 pl-2 pr-2 pt-1 pb-1 dark:text-white
+            className={`dark:bg-black hover:bg-white m-1 pl-2 pr-2 pt-1 pb-1 dark:text-white
             dark:hover:text-black font-mono rounded-[10px] w-5/12
             border-[1px] border-neutral-800 ${useTransition ? 'transition-all' : 'transition-none'}`} 
             {...props}> {text}
