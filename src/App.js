b918f203cc05a53671c450e576385e607b883dbf
MohammedHamim <99599371+BlackdreamIO@users.noreply.github.com>
2023-11-27 14:16:30 +0600

    Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import "./App.css";
 import CrudApp from "./CrudApp";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
 
 export default function App() 
 {
     return (
-        <div className="h-screen bg-black">
-          <ThemeProvider theme={darkTheme}>
-              <CssBaseline />
-              <CrudApp/>
-          </ThemeProvider>
+        <div className="h-screen dark:bg-black">
+          <CrudApp/>
         </div>
     );
 }
--- src/CrudApp.jsx
+++ src/CrudApp.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import AuthPanel from './components/AuthPanel';
 import TodoPanel from './components/TodoPanel';
+import Navbar from './components/Navbar';
+import AuthPanel from './components/AuthPanel';
 
 export default function CrudApp() 
 {
     return (
         <div className='flex flex-row items-center justify-center'>
+            <Navbar/>
             <AuthPanel/>
         </div>
     )
