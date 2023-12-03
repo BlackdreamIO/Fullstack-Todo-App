88a6b2fd8ea2f07731776ce358fe8aada99f3252
MohammedHamim <99599371+BlackdreamIO@users.noreply.github.com>
2023-12-03 17:40:23 +0600

    RT

--- src/components/TodoListPanel.jsx
+++ src/components/TodoListPanel.jsx
@@ -4,12 +4,13 @@ import { Box, Stack } from '@mui/material';
 import { TodoListPanelNavbar } from './TodoListPanelNavbar';
 import { TodoItem } from './TodoItem';
 
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import { BottomNavigation, BottomNavigationAction } from '@mui/material';
 import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
 import RestoreIcon from '@mui/icons-material/Restore';
 import AssignmentTurnedInOutlinedIcon from '@mui/icons-material/AssignmentTurnedInOutlined';
 import HourglassEmptyOutlinedIcon from '@mui/icons-material/HourglassEmptyOutlined';
+
+import { CheckIfDarkMode } from '../function/themeManager';
 
 export default function TodoListPanel() 
 {
@@ -18,6 +19,10 @@ export default function TodoListPanel()
     const handleChange = (event, newValue) => {
       setValue(newValue);
     };
+
+    const BottomNavigationActionStyle = 'bg-black hover:text-black dark:hover:bg-white dark:hover:text-black dark:text-neutral-500 dark:focus:text-white dark:focus:hover:text-black'
+
+    const isDarkMode = document.documentElement.className === 'dark' ? true : false;
 
     return (
         <Box className='w-9/12 bg-neutral-200 dark:bg-[rgb(5,5,5)] h-screen dark:border-neutral-800'>
@@ -28,10 +33,10 @@ export default function TodoListPanel()
             </Stack>
 
             <BottomNavigation className='w-full dark:bg-[rgb(5,5,5)]' value={value} onChange={handleChange}>
-                <BottomNavigationAction className='dark:text-neutral-500 dark:focus:text-white dark:target:text-cyan-500' label="Recents" value="recents" icon={<RestoreIcon />} />
-                <BottomNavigationAction className='dark:text-neutral-500 dark:focus:text-white' label="Complete" value="Completed" icon={<AssignmentTurnedInOutlinedIcon />} />
-                <BottomNavigationAction className='dark:text-neutral-500 dark:focus:text-white' label="Pending" value="Pending" icon={<HourglassEmptyOutlinedIcon />} />
-                <BottomNavigationAction className='dark:text-neutral-500 dark:focus:text-white' label="Archive" value="Archive" icon={<ArchiveOutlinedIcon />} />
+                <BottomNavigationAction sx={{ '&.Mui-selected': { color: isDarkMode ? 'white' : 'black' }}} fontSize={"0.8rem"} className={BottomNavigationActionStyle} label="Recents" value="recents" icon={<RestoreIcon />} />
+                <BottomNavigationAction sx={{ '&.Mui-selected': { color: isDarkMode ? 'white' : 'black' }}} fontSize={"0.8rem"} className={BottomNavigationActionStyle} label="Complete" value="Completed" icon={<AssignmentTurnedInOutlinedIcon />} />
+                <BottomNavigationAction sx={{ '&.Mui-selected': { color: isDarkMode ? 'white' : 'black' }}} fontSize={"0.8rem"} className={BottomNavigationActionStyle} label="Pending" value="Pending" icon={<HourglassEmptyOutlinedIcon />} />
+                <BottomNavigationAction sx={{ '&.Mui-selected': { color: isDarkMode ? 'white' : 'black' }}} fontSize={"0.8rem"} className={BottomNavigationActionStyle} label="Archive" value="Archive" icon={<ArchiveOutlinedIcon />} />
             </BottomNavigation>
         </Box>
     )
