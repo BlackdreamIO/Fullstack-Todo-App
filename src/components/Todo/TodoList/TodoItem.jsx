35fff73eafc20eb75e77a5d2e7301c55043625c0
MohammedHamim <99599371+BlackdreamIO@users.noreply.github.com>
2023-12-10 20:30:29 +0600

    .

--- src/components/Todo/TodoList/TodoItem.jsx
+++ src/components/Todo/TodoList/TodoItem.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Card, Typography, Stack, Box } from '@mui/material';
 
 export const TodoItem = () => 
 {
     return (
-      <div>
-        
-      </div>
+        <Box className='p-3 dark:bg-black dark:border-neutral-500 border-[1px] m-2' flexWrap={1}>
+            <Stack>
+                <Typography variant='h6' className='dark:text-white dark:text-base'>
+                    MAKE A FULLSTACK TODO App
+                </Typography>
+            </Stack>
+        </Box>
     )
 }
